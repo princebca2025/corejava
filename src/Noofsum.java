7dff7e2da037fb1f7487ef0a4918a608c9cbee03
princebca2025 <princeabhirana@gmail.com>
2026-05-21 16:00:56 +0530

    add sum of no file

---
+++ src/Noofsum.java
@@ -0,0 +1,10 @@
+public class Noofsum {
+    public static void main(String[] args) {
+        int a=50,b=58,c;
+        c=a*b;
+        a=c+b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
